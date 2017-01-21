8dfa2ed5f5cd11d656f83d7420184a6bda1cd91c
ylin10 <ylin10@bu.edu>
2017-01-20 20:46:01 -0500

    JQuery/Ajax Tutorial
    Test out of no-reload Jquery/ajax setup, want to be able to instantly
    compute speed limit difference without having to refresh server.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170120170649) do
+ActiveRecord::Schema.define(version: 20170121014446) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "speedcalcs", force: :cascade do |t|
     t.integer  "speedlimit"
